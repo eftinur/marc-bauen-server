da7f13b9f7780782c0e8661d268bfe6d1567c322
eftinur <eftinur90@gmail.comm>
2022-11-09 06:52:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`listening to ${port}`);
+})
+
+app.get('/', (req, res) => {
+    res.send('marc bauen server running')
+})
+
+// MongoDB
+
+// bauenAdmin nZuFJUJiUz9a0qXM
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tyocyp7.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+
+        const serviceCollection = client.db('bauen').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const Services = await cursor.limit(3).toArray();
+            res.send(Services);
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
